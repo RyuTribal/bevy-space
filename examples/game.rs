e0f8fee698d76d62c7d40ef584fd94dac4a5af3d
Per Lindgren <per.lindgren@ltu.se>
2024-09-30 05:57:09 +0200

    lazer works

--- examples/game.rs
+++ examples/game.rs
@@ -4,6 +4,8 @@
 use bevy::prelude::*;
 
 const PLAYER_SPEED: f32 = 300.0;
+const PLAYER_HEIGHT: f32 = 50.0; // There should be a way to get this from sprite
+const LAZER_SPEED: f32 = 500.0;
 const SCENE_WIDTH: f32 = 400.0;
 const SCENE_HEIGHT: f32 = 300.0;
 
@@ -17,9 +19,11 @@ enum Direction {
 #[derive(Component)]
 struct Player;
 
-#[derive(Component)]
-struct Lazer {
-    is_fired: bool,
+#[derive(Component, PartialEq, Clone, Copy)]
+enum Lazer {
+    Fire,
+    Fired,
+    Idle,
 }
 
 /// keyboard input
@@ -42,44 +46,14 @@ fn keyboard_input_system(
     }
 
     for mut lazer in &mut lazer_match {
-        if !lazer.is_fired
+        if *lazer == Lazer::Idle
             && (keyboard_input.just_pressed(KeyCode::Space)
                 || keyboard_input.pressed(KeyCode::ArrowUp))
         {
-            info!("' ' ^");
-            lazer.is_fired = true;
+            trace!("' ' ^");
+            *lazer = Lazer::Fire;
         }
     }
-}
-
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands.spawn(Camera2dBundle::default());
-    commands.spawn((
-        SpriteBundle {
-            texture: asset_server.load("sprites/space.png"),
-            transform: Transform::from_xyz(0., -SCENE_HEIGHT, 0.),
-            ..default()
-        },
-        Player,
-        Direction::None,
-    ));
-    commands.spawn((
-        SpriteBundle {
-            texture: asset_server.load("sprites/lazer.png"),
-            transform: Transform::from_xyz(0., SCENE_HEIGHT, 0.),
-            ..default()
-        },
-        Lazer { is_fired: false },
-    ));
-
-    commands.spawn((
-        SpriteBundle {
-            texture: asset_server.load("sprites/alien.png"),
-            transform: Transform::from_xyz(0., SCENE_HEIGHT, 0.),
-            ..default()
-        },
-        Direction::None,
-    ));
 }
 
 /// player movement
@@ -99,16 +73,77 @@ fn player_movement(
                     transform.translation.x += PLAYER_SPEED * time.delta_seconds()
                 }
             }
-
-            Direction::None => {}
+            _ => {}
         }
     }
+}
+
+/// lazer movement
+fn lazer_movement(
+    time: Res<Time>,
+    player_position: Query<&Transform, With<Player>>,
+    mut lazer_position: Query<(&mut Lazer, &mut Visibility, &mut Transform), Without<Player>>,
+) {
+    let player_position = player_position.iter().next().unwrap();
+    for (mut lazer, mut visibility, mut transform) in &mut lazer_position {
+        match *lazer {
+            Lazer::Fire => {
+                transform.translation =
+                    player_position.translation + Vec3::new(0.0, PLAYER_HEIGHT, 0.0);
+                *lazer = Lazer::Fired;
+                *visibility = Visibility::Visible;
+            }
+            Lazer::Fired => {
+                if transform.translation.y > SCENE_HEIGHT {
+                    *lazer = Lazer::Idle;
+                    *visibility = Visibility::Hidden;
+                } else {
+                    transform.translation.y += LAZER_SPEED * time.delta_seconds()
+                }
+            }
+            _ => {}
+        }
+    }
+}
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(Camera2dBundle::default());
+    commands.spawn((
+        SpriteBundle {
+            texture: asset_server.load("sprites/space.png"),
+            transform: Transform::from_xyz(0., -SCENE_HEIGHT, 0.),
+            ..default()
+        },
+        Player,
+        Direction::None,
+    ));
+    commands.spawn((
+        Lazer::Idle,
+        SpriteBundle {
+            texture: asset_server.load("sprites/lazer.png"),
+
+            transform: Transform::from_xyz(0., SCENE_HEIGHT, 0.),
+            visibility: Visibility::Hidden,
+            ..default()
+        },
+    ));
+
+    commands.spawn((
+        Direction::None,
+        SpriteBundle {
+            texture: asset_server.load("sprites/alien.png"),
+            transform: Transform::from_xyz(0., SCENE_HEIGHT, 0.),
+            ..default()
+        },
+    ));
 }
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup)
-        .add_systems(Update, (keyboard_input_system, player_movement))
+        .add_systems(
+            Update,
+            (keyboard_input_system, player_movement, lazer_movement),
+        )
         .run();
 }
