6bc132f3ca26e84c833a0ad6ef2ffaa864c43402
Per Lindgren <per.lindgren@ltu.se>
2024-10-01 01:05:37 +0200

    small update

--- src/main.rs
+++ src/main.rs
@@ -263,12 +263,12 @@ fn setup(
     }
     commands.spawn_batch(aliens);
 
-    // Builds and spawns the Defense spites
+    // Builds and spawns the bunker sprites
     let texture = asset_server.load("sprites/defense.png");
     let layout = TextureAtlasLayout::from_grid(UVec2::splat(16), 4, 2, None, None);
     let texture_atlas_layout = texture_atlas_layouts.add(layout);
 
-    //
+    // The sprite index layout of the bunker
     let bunker_matrix = [
         [0, 1, 1, 1, 1, 2],
         [1, 1, 1, 1, 1, 1],
